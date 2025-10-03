866660f1d03b4fe05605065a4de2e2b296f8c25c
s.eskandarian <sajadsoft1@gmail.com>
2025-10-03 20:05:49 +0330

    Refactor CourseSeeder to optimize course template handling
    - Simplified the course template creation by storing the result of StoreCourseTemplateAction in a variable.
    - Updated the seeder to use the stored template when creating courses, improving efficiency.
    - Adjusted formatting in karno_lms.php for consistency in category_id retrieval and session time representation.

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -14,28 +14,26 @@ class CourseSeeder extends Seeder
     /** Run the database seeds. */
     public function run(): void
     {
-        $data = require database_path('seeders/data/karno_lms.php');
-        foreach ($data['course_template'] as $row) {
-            StoreCourseTemplateAction::run([
-                'slug'        => StringHelper::slug($row['title']),
-                'title'       => $row['title'],
-                'description' => $row['description'],
-                'body'        => $row['body'],
-                'category_id' => $row['category_id'],
-                'level'       => $row['level'],
-                'type'        => $row['type'],
-                'sessions'    => $row['sessions'],
-            ]);
-        }
+        $data     = require database_path('seeders/data/karno_lms.php');
+        $row      = $data['course_template'][0];
+        $template = StoreCourseTemplateAction::run([
+            'slug'        => StringHelper::slug($row['title']),
+            'title'       => $row['title'],
+            'description' => $row['description'],
+            'body'        => $row['body'],
+            'category_id' => $row['category_id'],
+            'level'       => $row['level'],
+            'type'        => $row['type'],
+            'sessions'    => $row['sessions'],
+        ]);
 
         foreach ($data['course'] as $row) {
-            StoreCourseAction::run([
-                'course_template_id' => $row['course_template_id'],
-                'term_id'            => $row['term_id'],
-                'teacher_id'         => $row['teacher_id'],
-                'price'              => $row['price'],
-                'capacity'           => $row['capacity'],
-                'sessions'           => $row['sessions'],
+            StoreCourseAction::run($template, [
+                'term_id'    => $row['term_id'],
+                'teacher_id' => $row['teacher_id'],
+                'price'      => $row['price'],
+                'capacity'   => $row['capacity'],
+                'sessions'   => $row['sessions'],
             ]);
         }
     }
