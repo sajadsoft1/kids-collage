6a9b92b5ecc216bd5b1f2cbd4824fc4549b367ce
s.eskandarian <sajadsoft1@gmail.com>
2025-10-03 20:12:49 +0330

    Update admin routes for authentication and modular file inclusion
    - Changed logout method to use the 'web' guard for better security.
    - Replaced dynamic file inclusion with explicit requires for improved clarity and maintainability in the admin routes.

--- routes/admin.php
+++ routes/admin.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('admin/auth/login', LoginPage::class)->name('admin.auth.login');
 Route::get('admin/auth/logout', function () {
-    auth()->logout();
+    auth()->guard('web')->logout();
 
     return redirect()->route('admin.auth.login');
 })->name('admin.auth.logout');
@@ -25,8 +25,37 @@ Route::group(['middleware' => ['admin.panel']], function () {
 
     Route::get('utilitys/translate/{class}/{id}', DynamicTranslate::class)->name('admin.dynamic-translate');
     Route::get('utility/seo/{class}/{id}', DynamicSeo::class)->name('admin.dynamic-seo');
-    $files = array_diff(scandir(__DIR__ . '/admin', SCANDIR_SORT_ASCENDING), ['.', '..']);
-    foreach ($files as $file_name) {
-        require_once sprintf('admin/%s', $file_name);
-    }
+    require __DIR__ . '/admin/apps.php';
+    require __DIR__ . '/admin/attendance.php';
+    require __DIR__ . '/admin/banner.php';
+    require __DIR__ . '/admin/blog.php';
+    require __DIR__ . '/admin/bulletin.php';
+    require __DIR__ . '/admin/category.php';
+    require __DIR__ . '/admin/client.php';
+    require __DIR__ . '/admin/comment.php';
+    require __DIR__ . '/admin/contactUs.php';
+    require __DIR__ . '/admin/course.php';
+    require __DIR__ . '/admin/courseSession.php';
+    require __DIR__ . '/admin/courseSessionTemplate.php';
+    require __DIR__ . '/admin/courseTemplate.php';
+    require __DIR__ . '/admin/enrollment.php';
+    require __DIR__ . '/admin/faq.php';
+    require __DIR__ . '/admin/installment.php';
+    require __DIR__ . '/admin/license.php';
+    require __DIR__ . '/admin/notifications.php';
+    require __DIR__ . '/admin/opinion.php';
+    require __DIR__ . '/admin/order.php';
+    require __DIR__ . '/admin/page.php';
+    require __DIR__ . '/admin/payment.php';
+    require __DIR__ . '/admin/portFolio.php';
+    require __DIR__ . '/admin/role.php';
+    require __DIR__ . '/admin/room.php';
+    require __DIR__ . '/admin/seoOption.php';
+    require __DIR__ . '/admin/slider.php';
+    require __DIR__ . '/admin/socialMedia.php';
+    require __DIR__ . '/admin/tag.php';
+    require __DIR__ . '/admin/teammate.php';
+    require __DIR__ . '/admin/term.php';
+    require __DIR__ . '/admin/ticket.php';
+    require __DIR__ . '/admin/user.php';
 });
