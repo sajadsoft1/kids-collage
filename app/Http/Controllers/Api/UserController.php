a18d4fbd1d0e6de4f825da1341ee799a9953f144
Ahmad Dehestani <s.ahmad.dehestani@gmail.com>
2025-10-14 21:27:25 +0330

    userDeatails

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
+use App\Enums\BooleanEnum;
+use App\Enums\UserTypeEnum;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserDetailResource;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
@@ -15,19 +18,29 @@ use OpenApi\Annotations as OA;
 class UserController extends Controller
 {
     /** Display a listing of the resource. */
-    public function index() {}
+    public function index()
+    {
+    }
 
     /** Store a newly created resource in storage. */
-    public function store(Request $request) {}
+    public function store(Request $request)
+    {
+    }
 
     /** Display the specified resource. */
-    public function show(string $id) {}
+    public function show(string $id)
+    {
+    }
 
     /** Update the specified resource in storage. */
-    public function update(Request $request, string $id) {}
+    public function update(Request $request, string $id)
+    {
+    }
 
     /** Remove the specified resource from storage. */
-    public function destroy(string $id) {}
+    public function destroy(string $id)
+    {
+    }
 
     /**
      * @OA\Get(
@@ -61,4 +74,37 @@ class UserController extends Controller
             ]
         );
     }
+
+    /**
+     * @OA\Get(
+     *     path="/teacher/{user}",
+     *     operationId="getUserByID",
+     *     tags={"User"},
+     *     summary="Get user information",
+     *     description="Returns user data",
+     *     @OA\Parameter(name="user", required=true, in="path", @OA\Schema(type="integer")),
+     *     @OA\Response(response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(type="object",
+     *             @OA\Property(property="message", type="string", default="No message"),
+     *             @OA\Property(property="data", ref="#/components/schemas/UserDetailResource")
+     *         )
+     *     )
+     * )
+     */
+    public function showTeacher(User $user): JsonResponse
+    {
+
+        if ($user->type !==UserTypeEnum::TEACHER || $user->status != BooleanEnum::ENABLE) {
+            abort(404);
+        }
+
+        return Response::data(
+            [
+                'teacher' => UserDetailResource::make($user->load(['profile'])),
+            ]
+        );
+    }
 }
+
+
