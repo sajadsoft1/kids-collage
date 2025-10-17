99212de1591c302a89c594ad185cf62e37f3488b
sajad.es <sajadsoft1@gmail.com>
2025-10-17 14:55:11 +0330

    update composer and env example

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,10 +1,9 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
